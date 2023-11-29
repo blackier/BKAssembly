390391c6a665934a6eebb8970128b68696b499e5
blackier <fengxiyulei@sina.com>
2023-11-29 22:38:44 +0800

    feat: add HttpClientExtensions

---
+++ BKAssembly/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKAssembly.Extensions;
+
+public static class HttpClientExtensions
+{
+    public  static bool TrySend<T>(this HttpClient httpClient,HttpRequestMessage request, out HttpResponseMessage response, out T result)
+    {
+        try
+        {
+            response = httpClient.Send(request);
+        }
+        catch {
+            response = null;
+            result = default(T);
+            return false;
+        }
+        try
+        {
+            result = BKMisc.JsonDeserialize<T>(response.Content.ReadAsStringAsync().Result);
+            return true;
+        }
+        catch
+        {
+            result = default(T);
+            return false;
+        }
+    }
+}
