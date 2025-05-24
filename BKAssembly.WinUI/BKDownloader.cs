cbb25c1f524afa7d1de95576ff4f37b46e81f2ac
blackier <blackier@users.noreply.github.com>
2025-05-24 19:32:44 +0800

    perf(WinUI): BKDownloader添加Accept和AcceptEncoding，每次下载单独一个HttpClient

--- BKAssembly.WinUI/BKDownloader.cs
+++ BKAssembly.WinUI/BKDownloader.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,8 +16,6 @@ namespace BKAssembly.WinUI;
 
 public class BKDownloader
 {
-    private static HttpClient _httpClient;
-
     public static IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress> DownloadFile(
         Uri uri,
         Action<IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress>, HttpProgress> progress,
@@ -27,20 +24,27 @@ public class BKDownloader
     {
         // https://www.cnblogs.com/webabcd/p/3213590.html
         // https://learn.microsoft.com/zh-cn/windows/uwp/networking/httpclient
-        if (_httpClient == null)
-        {
-            var filter = new HttpBaseProtocolFilter();
-            filter.CacheControl.ReadBehavior = HttpCacheReadBehavior.MostRecent;
-            _httpClient = new HttpClient(filter);
-            _httpClient.DefaultRequestHeaders.UserAgent.Clear();
-            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.95 Safari/537.36 QIHU 360EE"
+        HttpClient httpClient = new();
+        //var filter = new HttpBaseProtocolFilter();
+        //filter.CacheControl.ReadBehavior = HttpCacheReadBehavior.MostRecent;
+        httpClient =
+            new HttpClient( /*filter*/
             );
-        }
+        httpClient.DefaultRequestHeaders.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Accept.TryParseAdd(
+            "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8"
+        );
+        httpClient.DefaultRequestHeaders.AcceptEncoding.Clear();
+        httpClient.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("gzip, deflate, br");
+        httpClient.DefaultRequestHeaders.UserAgent.Clear();
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.95 Safari/537.36"
+            );
 
         HttpRequestMessage message = new() { RequestUri = uri };
 
-        var result = _httpClient.SendRequestAsync(message);
+        var result = httpClient.SendRequestAsync(message);
         result.Progress = (asyncInfo, progressInfo) =>
         {
             progress(asyncInfo, progressInfo);
